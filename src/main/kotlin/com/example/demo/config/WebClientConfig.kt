e24d86ecfaed9426f247f975d6f26b2ad385853b
waveaway77 <waveaway77@knou.ac.kr>
2026-01-13 11:23:02 +0900

    spring boot test 주석처리

---
+++ src/main/kotlin/com/example/demo/config/WebClientConfig.kt
@@ -0,0 +1,14 @@
+package com.example.demo.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
+
+@Configuration
+class WebClientConfig() {
+
+    @Bean
+    fun webClient(): WebClient {
+        return webClient()
+    }
+}
